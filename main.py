b8c79bd94cda826bc43ec1e83af5dfb194cdf8c8
Lee-David-88 <leedavid19990430@gmail.com>
2023-01-13 23:58:53 -0800

    Add recording and playing keystrokes

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-import keyboard as kb
+import keyboard
+import mouse
+
+
+def start_recording(_speed):
+    while True:
+        if keyboard.is_pressed('enter'):
+            record = keyboard.record(until="esc")
+            keyboard.wait("shift")
+            play_recording(record, _speed)
+
+
+def play_recording(_record, _speed):
+    if _record != "":
+        keyboard.play(_record, speed_factor=_speed)
+    else:
+        print("Empty Record")
+
+
+if __name__ == '__main__':
+    speed = 1
+    start_recording(speed)
