4e0700c54c9a3d58ab641b364b760c92d74e26eb
Lee-David-88 <leedavid19990430@gmail.com>
2023-01-14 14:00:16 -0800

    Add function for mouse recording

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ import keyboard
 import mouse
 
 
-def start_recording(_speed):
+def start_keyboard_recording(_speed):
     while True:
         if keyboard.is_pressed('enter'):
             record = keyboard.record(until="esc")
             keyboard.wait("shift")
-            play_recording(record, _speed)
+            play_keyboard_recording(record, _speed)
 
 
-def play_recording(_record, _speed):
+def play_keyboard_recording(_record, _speed):
     if _record != "":
         keyboard.play(_record, speed_factor=_speed)
     else:
         print("Empty Record")
 
 
+def start_mouse_recording():
+    return
+
+
 if __name__ == '__main__':
     speed = 1
-    start_recording(speed)
+    start_keyboard_recording(speed)
